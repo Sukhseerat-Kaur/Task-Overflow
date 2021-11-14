ae2e509b229dab60177aa17694a7086ea6943d4d
Sukhseerat-Kaur <karanjitkaur78@gmail.com>
2021-11-14 21:15:47 +0530

    added page header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
-import Board from "./components/Board";
 
 function App() {
-  return <div>Working</div>;
+  return (
+    <div className="App font-Rajdhani w-screen h-screen bg-yellow-50 flex flex-col">
+      <header className="bg-blue-900 w-full h-16 flex justify-center items-center text-5xl text-orange p-9">
+        TASK OVERFLOW
+      </header>
+    </div>
+  );
 }
 
 export default App;
